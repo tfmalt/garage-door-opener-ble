cadcb04c3abd7e6b53c1ea25bd61a84b933e7d1e
Thomas Malt <thomas@malt.no>
2015-01-10 14:20:46 +0100

    Initial Commit

---
+++ GarageOpener/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GarageOpener
+//
+//  Created by Thomas Malt on 10/01/15.
+//  Copyright (c) 2015 Thomas Malt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
