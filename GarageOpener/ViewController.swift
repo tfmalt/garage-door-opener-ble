f99774bbaa00960f717be13cec47ef2d089630b9
Thomas Malt <thomas@malt.no>
2015-01-11 03:01:00 +0100

    Starting to flesh out the viewController.

--- GarageOpener/ViewController.swift
+++ GarageOpener/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    @IBOutlet weak var textLog: UITextView!
+    
+    var counter = 0
+    var discovery : AnyObject?
+    var nc = NSNotificationCenter.defaultCenter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textLog.text = ""
+        self.addLogMsg("Starting...");
+        
+        nc.addObserver(self, selector: Selector("bleStateChanged:"), name: "bleStateChangedNotification", object: nil)
+        discovery = BTDiscovery()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func openButtonPressed(sender: UIButton) {
+        counter = counter + 1;
+        
+        self.addLogMsg("button push \(counter)")
+        
+    }
+    
+    func addLogMsg(msg: String) {
+        textLog.text = textLog.text + "\n" + msg
+        var length = countElements(textLog.text);
+        var range = NSMakeRange(length - 1, 1);
+        textLog.scrollRangeToVisible(range);
+    }
+    
+    func bleStateChanged(notification: NSNotification) {
+        var msg = notification.object as String
+        println("got notification: \(msg)")
+        dispatch_async(dispatch_get_main_queue(), {
+            self.addLogMsg(msg)
+        })
+    }
 }
 
