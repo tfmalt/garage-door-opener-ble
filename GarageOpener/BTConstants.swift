9354d60a7ca4b376aa7125b9df104e0018d4b1e0
Thomas Malt <thomas@malt.no>
2015-01-12 02:15:18 +0100

    Created a constants struct, because it seemed to make sense.

---
+++ GarageOpener/BTConstants.swift
@@ -0,0 +1,17 @@
+//
+//  BTConstants.swift
+//  GarageOpener
+//
+//  Created by Thomas Malt on 11/01/15.
+//  Copyright (c) 2015 Thomas Malt. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+struct BTConstants {
+    let SERVICE_UUID     = "713D0000-503E-4C75-BA94-3148F18D941E"
+    let IDENTIFIER_UUID  = "535E0D00-7ABE-E6BD-2D0F-97D539053FB0"
+    let CHAR_TX_UUID     = "713D0002-503E-4C75-BA94-3148F18D941E"
+    let CHAR_RX_UUID     = "713D0003-503E-4C75-BA94-3148F18D941E"
+}
