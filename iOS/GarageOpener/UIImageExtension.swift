13345e217a58dc7d6868f83f4a27c42a993f5eea
Thomas Malt <thomas@malt.no>
2015-01-14 01:05:11 +0100

    extensions in swift are fantastic. added extension to create uiimage from color

---
+++ iOS/GarageOpener/UIImageExtension.swift
@@ -0,0 +1,39 @@
+//
+//  UIImageExtension.swift
+//  GarageOpener
+//
+//  Created by Thomas Malt on 13/01/15.
+//  Copyright (c) 2015 Thomas Malt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    class func imageWithColor(color:UIColor?) -> UIImage! {
+        
+        let rect = CGRectMake(0.0, 0.0, 1.0, 1.0);
+        
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        
+        let context = UIGraphicsGetCurrentContext();
+        
+        if let color = color {
+            
+            color.setFill()
+        }
+        else {
+            
+            UIColor.whiteColor().setFill()
+        }
+        
+        CGContextFillRect(context, rect);
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return image;
+    }
+    
+}
