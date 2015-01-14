f9279beab227c261795bd6b3dae77d6c5c809326
Thomas Malt <thomas@malt.no>
2015-01-14 01:05:53 +0100

    Added extension to uicolor to let me create a color from a hex value.

---
+++ iOS/GarageOpener/UIColorExtension.swift
@@ -0,0 +1,54 @@
+//
+//  UIColorExtension.swift
+//  GarageOpener
+//
+//  Created by Thomas Malt on 13/01/15.
+//  Copyright (c) 2015 Thomas Malt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    class func colorWithHex(hexString: String?) -> UIColor? {
+        
+        return colorWithHex(hexString, alpha: 1.0)
+    }
+    
+    class func colorWithHex(hexString: String?, alpha: CGFloat) -> UIColor? {
+        
+        if let hexString = hexString {
+            
+            var error : NSError? = nil
+            
+            let regexp = NSRegularExpression(pattern: "\\A#[0-9a-f]{6}\\z",
+                options: .CaseInsensitive,
+                error: &error)
+            
+            let count = regexp?.numberOfMatchesInString(hexString,
+                options: .ReportProgress,
+                range: NSMakeRange(0, countElements(hexString)))
+            
+            if count != 1 {
+                
+                return nil
+            }
+            
+            var rgbValue : UInt32 = 0
+            
+            let scanner = NSScanner(string: hexString)
+            
+            scanner.scanLocation = 1
+            scanner.scanHexInt(&rgbValue)
+            
+            let red   = CGFloat( (rgbValue & 0xFF0000) >> 16) / 255.0
+            let green = CGFloat( (rgbValue & 0xFF00) >> 8) / 255.0
+            let blue  = CGFloat( (rgbValue & 0xFF) ) / 255.0
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        }
+        
+        return nil
+    }
+}
