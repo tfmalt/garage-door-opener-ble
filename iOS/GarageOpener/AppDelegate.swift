2518766bf65ef67e2907917b3b911e45925bca74
Thomas Malt <thomas@malt.no>
2015-01-16 23:51:11 +0100

    Added settings defaults.

--- iOS/GarageOpener/AppDelegate.swift
+++ iOS/GarageOpener/AppDelegate.swift
@@ -16,12 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+
+        var appDefaults = NSUserDefaults.standardUserDefaults()
+        appDefaults.registerDefaults([
+            "showPassword": false,
+            "password": "default"
+        ])
         return true
     }
 
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
@@ -39,6 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        println("got will terminate")
     }
 
 
