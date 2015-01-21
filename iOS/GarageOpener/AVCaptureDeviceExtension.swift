346c64b41128f0154ebfc61255d8d0d285ed6997
Thomas Malt <thomas@malt.no>
2015-01-21 14:12:15 +0100

    A class extension to AVCaptureDevice that just fetches the front facing camera.

---
+++ iOS/GarageOpener/AVCaptureDeviceExtension.swift
@@ -0,0 +1,30 @@
+//
+//  AVCaptureDeviceExtension.swift
+//  GarageOpener
+//
+//  Created by Thomas Malt on 21/01/15.
+//  Copyright (c) 2015 Thomas Malt. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+extension AVCaptureDevice {
+    
+    /// Function to retrieve the first forward facing camera we find.
+    /// 
+    /// There is probably only one camera like that on a device, and
+    /// if there are several the first found will be good engugh.
+    class func deviceWithVideoInFront() -> AVCaptureDevice? {
+        let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        
+        for device in devices {
+            if device.position == AVCaptureDevicePosition.Front {
+                return device as? AVCaptureDevice
+            }
+        }
+        
+        // If we get this far we return nil
+        return nil
+    }
+}
